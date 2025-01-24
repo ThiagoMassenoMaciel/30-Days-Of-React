647cbc8d39b57197f38a9329a5c61c9afbddeb73
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2025-01-24 18:28:42 -0300

    full : Day 7: Class Components

---
+++ solutions/day-07/exercises/README.md
@@ -0,0 +1,15 @@
+# Exercises from day 07 [link]()
+## Day 7: Class Components
+
+#### Perguntas Exercises: Level 1
+###### How do you write a pure JavaScript function
+###### What is inheritance and how do you make a child from a parent class?
+###### What is class based React component ?
+###### What is the difference between functional React component and class based React component ?
+###### When do we need to use class based components instead of functional components
+###### What is the use cases of class based component ?
+###### Which type of component do use most frequently ? functional or class-based component
+###### What is React life cycle ? (not covered yet) ?
+###### What is state in React ? (not covered yet)
+
+#### Não farei os exercicios pois componente de classe é das versões antigas , o cara do curso recomendou eu conhecer tanto o jeito novo como antigo na possibilidade talvez de um dia eu mexer em algum código react velho e ter que atualizar para o react recent
---
+++ solutions/day-07/understanding/examples/src/App.jsx
@@ -0,0 +1,675 @@
+import React from 'react'
+
+import PropTypes from 'prop-types';
+
+// Pure JavaScript class and child
+// Imagine this what we import from React package
+class Component {
+  constructor(props) {}
+}
+
+// This how we make class based components by inheriting from the parent
+class Child extends Component {
+  constructor(props) {
+    super(props)
+  }
+}
+
+/*
+
+--Header Component Functional component--
+
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 6, 2020</small>
+    </div>
+  </header>
+)
+
+*/
+
+//============================================================ class based component without a constructor
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>Welcome to 30 Days Of React</h1>
+          <h2>Getting Started React</h2>
+          <h3>JavaScript Library</h3>
+          <p>Asabeneh Yetayeh</p>
+          <small>Oct 7, 2020</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+
+//============================================================ class based component with a constructor
+
+
+class Header1 extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>Welcome to 30 Days Of React</h1>
+          <h2>Getting Started React</h2>
+          <h3>JavaScript Library</h3>
+          <p>Asabeneh Yetayeh</p>
+          <small>Oct 7, 2020</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+/*
+
+TechList Component functional component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+*/
+
+// TechList Component class base component
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+/*
+Main Component Functional Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+    </div>
+  </main>
+)
+*/
+
+// Main Component Class Component
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList />
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
+
+// Footer Component Functional component
+/*
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+*/
+
+// Footer Component  Class component
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright 2020</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+// The App, or the parent or the container Functional Component
+
+/*
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+*/ 
+
+// The App, or the parent or the container Class Component
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className='app'>
+        <Header1 />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+
+
+//==========================================================Accessing props in Class components
+// class based component
+class Header2 extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>{this.props.data.welcome}</h1>
+          <h2>{this.props.data.title}</h2>
+          <h3>
+            {this.props.data.author.firstName} {this.props.data.author.lastName}
+          </h3>
+          <small>{this.props.data.date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+Header2.propTypes = {
+  data: PropTypes.shape({
+    welcome: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+    }).isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const App1 = () => {
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+    date: 'Oct 7, 2020',
+  }
+
+  return (
+    <div className='app'>
+      <Header2 data={data} />
+    </div>
+  )
+}
+
+//======================= in order to reapeat  props.data, use destructuring the hole object 
+
+class Header3 extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+Header3.propTypes = {
+  data: PropTypes.shape({
+    welcome: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    author: PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+    }).isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const App2 = () => {
+  const data = {
+    welcome: 'Welcome to 30 Days Of React',
+    title: 'Getting Started React',
+    subtitle: 'JavaScript Library',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+    date: 'Oct 6, 2020',
+  }
+
+  return (
+    <div className='app'>
+      <Header3 data={data} />
+    </div>
+  )
+}
+
+
+//======================= use destructuring the hole object for the rest class component 
+
+// TechList Component class base component
+class TechList1 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+TechList1.propTypes = {
+  techs: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+// Main Component Class Component
+class Main1 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList1 techs={this.props.techs} />
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
+
+Main1.propTypes = {
+  techs: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+// Footer Component Class component
+class Footer1 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+Footer1.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+};
+
+class App3 extends React.Component {
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    return (
+      <div className='app'>
+        <Header3 data={data} />
+        <Main1 techs={techs} />
+        <Footer1 date={new Date()} />
+      </div>
+    )
+  }
+}
+
+//===================================================== Methods in Class based component
+
+// class based component
+class Header4 extends React.Component {
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+
+  buttonSTYLE = { backgroundColor:'#242424', border: '2px solid #fff',color:'white', width:'fit-content', height:'fit-content', padding:'10px'};
+  render() {
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>Welcome to 30 Days Of React</h1>
+          <h2>Getting Started React</h2>
+          <h3>JavaScript Library</h3>
+          <p>Asabeneh Yetayeh</p>
+          <small>Oct 7, 2020</small>
+          <button style={this.buttonSTYLE} onClick={this.greetPeople}> Greet </button>
+        </div>
+      </header>
+    )
+  }
+}
+
+class App4 extends React.Component {
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+
+    return (
+      <div className='app'>
+        <Header4 data={data} />
+      </div>
+    )
+  }
+}
+
+//=================================== Now, let's the code we had add all the necessary methods. 
+
+import foto from './images/foto.jpeg';
+
+// Fuction to show month date year
+
+// User Card Component
+const UserCard = ({ user: { firstName, lastName, image } }) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+
+UserCard.propTypes = {
+  user: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+// A button component
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.object,
+};
+
+// CSS styles in JavaScript Object
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
+
+// class based component
+class Header5 extends React.Component {
+  constructor(props) {
+    super(props)
+    // the code inside the constructor run before any other code
+  }
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+Header5.propTypes = {
+  data: PropTypes.shape({
+    welcome: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+    author: PropTypes.shape({
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+    }).isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+
+// TechList Component class base component
+class TechList2 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+TechList2.propTypes = {
+  techs: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+
+// Main Component Class Component
+class Main2 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList2 techs={this.props.techs} />
+          </ul>
+          <UserCard user={this.props.user} />
+          <Button
+            text='Greet People'
+            onClick={this.props.greetPeople}
+            style={buttonStyles}
+          />
+          <Button
+            text='Show Time'
+            onClick={this.props.handleTime}
+            style={buttonStyles}
+          />
+        </div>
+      </main>
+    )
+  }
+}
+
+Main2.propTypes = {
+  user: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  techs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  greetPeople: PropTypes.func.isRequired,
+  handleTime: PropTypes.func.isRequired,
+};
+
+
+// Footer Component Class component
+class Footer2 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+Footer2.propTypes = {
+  date: PropTypes.instanceOf(Date).isRequired,
+};
+
+class App5 extends React.Component {
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Thiago',
+        lastName: 'Masseno',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    // copying the author from data object to user variable using spread operator
+    const user = { ...data.author, image: foto }
+
+    return (
+      <div className='app'>
+        <Header5 data={data} />
+        <Main2
+          user={user}
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+        />
+
+        <Footer2 date={new Date()} />
+      </div>
+    )
+  }
+}
+
+export default App5;
